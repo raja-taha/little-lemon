d1f9ff9bde758ed89586e9f6695a41d14f6467df
Mr_RajaG <rajataha062@gmail.com>
2024-01-13 06:39:43 +0500

    version 0.1

--- src/App.js
+++ src/App.js
@@ -1,85 +1,20 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import BookingPage from "./pages/BookingPage";
+import ConfirmedBooking from "./pages/ConfirmedBooking";
 import "./App.css";
-import { useState, useEffect, React } from "react";
-import {
-  Navbar,
-  Header,
-  Main,
-  Footer,
-  Reservations,
-  OrderOnline,
-} from "./components";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { headerData, reservationData, orderOnline } from "./constants";
-import DataContext from "./DataContext";
-import ConfirmedBooking from "./components/Reservations/ConfirmedBooking";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  // use state to set data
-  // use effect to get path
-  const location = useLocation();
-  const [path, setPath] = useState(location.pathname);
-
-  // set path name on route change
-  useEffect(() => {
-    setPath(location.pathname);
-  }, [location.pathname]);
-
-  // Send data to header to based on route
-  let data = {};
-  if (path === "/" || path === "/Little-Lemon") {
-    data = headerData;
-  } else if (path === "/Reservations") {
-    data = reservationData;
-  } else if (path === "/OrderOnline") {
-    data = orderOnline;
-  }
-
   return (
     <>
-      <Navbar />
-      <DataContext.Provider value={data}>
-        <Routes>
-          <Route
-            path="/Little-Lemon"
-            element={
-              <>
-                <Header />
-                <Main />
-              </>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Main />
-              </>
-            }
-          />
-          <Route
-            path="/Reservations"
-            element={
-              <>
-                <Header />
-                <Reservations />
-              </>
-            }
-          />
-          <Route
-            path="/OrderOnline"
-            element={
-              <>
-                <Header />
-                <OrderOnline />
-              </>
-            }
-          />
-
-          {/* Path for booking confirmation */}
-          <Route path="/ConfirmedBooking" element={<ConfirmedBooking />} />
-        </Routes>
-      </DataContext.Provider>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<HomePage />}></Route>
+        <Route path="/booking" element={<BookingPage />}></Route>
+        <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Testimonials = () => {
+  return <div>Testimonial</div>;
+};
+
+export default Testimonials;
